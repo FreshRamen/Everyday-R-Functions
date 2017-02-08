287409e3599493f05db7c8d94cf98bc80c74ad7d
FreshRamen <FreshRamen@users.noreply.github.com>
2017-02-08 14:40:21 +0100

    Update JFCI.R
    Added description for the function

--- JFCI.R
+++ JFCI.R
@@ -1,4 +1,7 @@
 JFCI <- function(pos_words, neg_words){
+	# This function computes the Janis-Fadner Coefficient of Imbalance, for sentiment analysis.
+	# pos_words and neg_words are one-dimensional numeric vectors, indicating the number of 
+	# positive and negative words in a text, respectively.
 	p <- sum(pos_words)
 	n <- sum(neg_words)
 	if (p > n) {
