9ae9be3080fbc6636ca63b22165655ff06643c19
FreshRamen <al.tabq@yahoo.com>
2017-02-08 15:09:20 +0100

    Added some comments for logbook method

--- logbook.R
+++ logbook.R
@@ -1,4 +1,10 @@
+# These functions I use for logging. logbook.add() adds text to a vector, which is then, in the end, concentrated and written do disk as a markdown file by logbook.write().
+
 logbook.add <- function(..., append = T, collapse = T){
+  # Adds character vector(s) to a "logbook", a vector containing text to be logged. 
+  # Can be written to disk as markdown using logbook.write().
+  # Use append = F to overwrite existing logbook. If logbook is missing, the script will create one.
+  # Use collapse = F to store each element of ... as an individual line in the logbook (useful, e.g., for items in a list).
   if (!exists("logbook") | append == F) logbook <<- vector()
   if (collapse == T){
       logbook.new <- paste(c(...), collapse = " ")
