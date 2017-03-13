db3a27f5e6fb88c430e9be5d3711f76d9d792e05
FreshRamen <al.tabq@yahoo.com>
2017-03-13 21:16:57 +0100

    Improved write.zip(), added some fallbacks

--- write.zip.R
+++ write.zip.R
@@ -1,25 +1,32 @@
-write.zip <- function(x, file, path = getwd(), ...) {
-	## Function writes data to a zipped csv file. ##
-	# x -- Object to be written to CSV, then zipped
-	# file -- File name of the csv file (including ".csv")
-	# path (optional) -- part to write zip file to
-	# ... -- Other options passed to "write.csv()"
+write.zip <- function(x, file, ...) {
+	## Function writes data to a zipped csv file
+	# x -- data.table object to be written to CSV, then zipped
+	# file -- File name of the csv file
+	# ... -- Other options passed to "fwrite()"
 	## Parse file name
-	zipFile <- paste0(file,".zip")
-	## Store old path
-	oldPath <- getwd()
+  if (substr(file,nchar(file)-7,nchar(file)) == ".csv.zip") {
+    csvFile <- substr(file,1,nchar(file)-4)
+    zipFile <- file
+  } else if (substr(file,nchar(file)-3,nchar(file)) == ".csv") {
+    csvFile <- file
+    zipFile <- paste0(csvFile,".zip")
+  } else {
+    csvFile <- paste0(paste0(file,".csv"))
+    zipFile <- paste0(csvFile,".zip")
+  }   
+	oldPath <- getwd() 	# Store old path
 	## Create temporary directory, or flush if exists
-	if (!file.exists(tempdir())) {dir.create(tempdir())
-    } else {file.remove(list.files(tempdir(), full = T, pattern = "*.csv"))}	
-	setwd(tempdir())
-	## Write file to tempdir
-	write.csv(x, file = file, ...)
-	## Zip the file
-	zip(zipFile, files = file)
-	## Move file to working directry
-	file.copy(zipFile, file.path(path,zipFile), overwrite = T)
-	## Remove old file
-	file.remove(file.path(tempdir(),file))
+	if (!file.exists(tempdir())) {
+    dir.create(tempdir())
+  } else {
+    file.remove(list.files(tempdir(), full = T, pattern = "*.csv"))
+  }	
+	## Write file to tempdir, zip and move to oldPath
+  setwd(tempdir())
+	write.csv(x, file = csvFile, ...) 
+	zip(zipFile, files = csvFile)
+	file.copy(zipFile, file.path(oldPath,zipFile), overwrite = T)
+	file.remove(csvFile,zipFile)
 	## Return to old path
 	setwd(oldPath)
 }
