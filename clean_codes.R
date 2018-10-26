bb401a9bcf3f1533c136947561e684467357e718
FreshRamen <al.tabq@yahoo.com>
2018-10-26 17:26:37 +0200

    Clean_codes uses named lists, which is easier to edit, clean ups, added tonum.ch to handle swiss numeral conversion

--- clean_codes.R
+++ clean_codes.R
@@ -1,6 +1,9 @@
 clean_codes <- function(string){
   # Function cleans certain codes from a string
-  unwanted_array = list('<br />'='; ', '( ){2,}'='; ')
+  unwanted_array = list(
+    '<br />'='; ', # Remove <br... by ;
+    '( ){2,}'='; ' # Replace multiple spaces by "; "
+    )
   
   out <- string
   for(i in seq_along(unwanted_array))
